808583e08067074a154b3f9e16e50b9849ffd65f
--add <rahmanaky521@gmail.com>
2023-10-12 15:10:03 +0600

    error handle

--- routes/blogs.js
+++ routes/blogs.js
@@ -6,52 +6,72 @@ const prisma = new PrismaClient();
 
 // get Posts
 router.get("/blog", async (req, res) => {
-  const allpost = await prisma.post.findMany();
-  res.send(allpost);
+  try {
+    const allpost = await prisma.post.findMany();
+    res.status(200).send(allpost);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
 });
 
 // get single Posts
 router.get("/blog/:id", async (req, res) => {
   const { id } = req.params;
-  const singlepost = await prisma.post.findUnique({
-    where: {
-      id: id,
-    },
-  });
-  res.send(singlepost);
+  try {
+    const singlepost = await prisma.post.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    res.status(200).send(singlepost);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
 });
 
 // update single Posts
 router.put("/blog", async (req, res) => {
-  const UpdatedData = await req.body;
-  const updatepost = await prisma.post.update({
-    where: {
-      id: UpdatedData?.id,
-    },
-    data: {
-      title: UpdatedData?.title,
-      description: UpdatedData?.description,
-      image: UpdatedData?.image,
-    },
-  });
-  res.send(updatepost);
+  try {
+    const UpdatedData = await req.body;
+    const updatepost = await prisma.post.update({
+      where: {
+        id: UpdatedData?.id,
+      },
+      data: {
+        title: UpdatedData?.title,
+        description: UpdatedData?.description,
+        image: UpdatedData?.image,
+      },
+    });
+    res.send(updatepost);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
 });
 
 // create post
 router.post("/blog", async (req, res) => {
-  const newpost = await prisma.post.create({ data: req.body });
-  res.send(newpost);
+  try {
+    const newpost = await prisma.post.create({ data: req.body });
+    res.status(201).send(newpost);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
 });
 
 // Delete Post
 router.delete("/blog/:id", async (req, res) => {
   const { id } = req.params;
-  await prisma.post.delete({
-    where: {
-      id: id,
-    },
-  });
-  res.send({ message: "Blog Deleted" });
+  try {
+    await prisma.post.delete({
+      where: {
+        id: id,
+      },
+    });
+    res.send({ message: "Blog Deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
 });
 
 module.exports = router;
--- routes/events.js
+++ routes/events.js
@@ -6,52 +6,72 @@ const prisma = new PrismaClient();
 
 // get events
 router.get("/events", async (req, res) => {
-  const allpost = await prisma.events.findMany();
-  res.send(allpost);
+  try {
+    const allpost = await prisma.events.findMany();
+    res.status(200).send(allpost);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
 });
 
 // get single events
 router.get("/events/:id", async (req, res) => {
   const { id } = req.params;
-  const singlepost = await prisma.events.findUnique({
-    where: {
-      id: id,
-    },
-  });
-  res.send(singlepost);
+  try {
+    const singlepost = await prisma.events.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    res.status(200).send(singlepost);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
 });
 
 // update single events
 router.put("/events", async (req, res) => {
-  const UpdatedData = await req.body;
-  const updatepost = await prisma.events.update({
-    where: {
-      id: UpdatedData?.id,
-    },
-    data: {
-      title: UpdatedData?.title,
-      shortDescription: UpdatedData?.shortDescription,
-      image: UpdatedData?.image,
-    },
-  });
-  res.send(updatepost);
+  try {
+    const UpdatedData = await req.body;
+    const updatepost = await prisma.events.update({
+      where: {
+        id: UpdatedData?.id,
+      },
+      data: {
+        title: UpdatedData?.title,
+        shortDescription: UpdatedData?.shortDescription,
+        image: UpdatedData?.image,
+      },
+    });
+    res.send(updatepost);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
 });
 
 // create events
 router.post("/events", async (req, res) => {
-  const newpost = await prisma.events.create({ data: req.body });
-  res.send(newpost);
+  try {
+    const newpost = await prisma.events.create({ data: req.body });
+    res.status(201).send(newpost);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
 });
 
 // Delete events
 router.delete("/events/:id", async (req, res) => {
   const { id } = req.params;
-  await prisma.events.delete({
-    where: {
-      id: id,
-    },
-  });
-  res.send({ message: "Post Deleted" });
+  try {
+    await prisma.events.delete({
+      where: {
+        id: id,
+      },
+    });
+    res.send({ message: "Post Deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
 });
 
 module.exports = router;
